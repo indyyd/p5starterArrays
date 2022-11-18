9ef037dd7cb9dd0e45a7bca697f6e3c03aae8d2b
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-18 11:52:20 +0000

    Add files via upload
    arrays and ifs using words

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,11 @@
-let nums = [100, 75, 200, 87, 25]
+let words = ["Plymouth", "is", "awesome"]
 //each value in an array is called an 'element'
 //they are used so you can keep track of them
 //each element in an array has an index
 //the first element is called 0, and then they go up from there
 //we can access the value from an array by using the index
 //e.g. nums [0] to access '100' in that array
-let numIndex = 0;
+let wordIndex = 0;
 
 function setup() {
     var canvas = createCanvas(400, 400);
@@ -14,17 +14,18 @@ function setup() {
 
 function draw(){
     background(220);
-
-    fill(255, 255, 255);
-    circle(200, 200, nums[numIndex]);
+    textSize(mouseX);
+    text(words[wordIndex], mouseX, mouseY);
 }
 
-function keyPressed() {
-    if (numIndex == 4) {
-        numIndex = 0;
+function mousePressed () {
+    if(wordIndex == words.length-1) {
+    wordIndex = 0;
     } else {
-        numIndex ++;
+        wordIndex++;
     }
-    console.log('numIndex:' + numIndex);  
-    }
+console.log('wordIndex:' + wordIndex)
+}
+
+    
     
